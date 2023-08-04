a038e9dae6b1fc0db38ca0d5acc964b776633515
Ekaterina Aladyeva <46932077+aladyeva-wustl@users.noreply.github.com>
2023-08-04 11:47:08 -0500

    Update processed_qc.R

--- scripts/processed_qc.R
+++ scripts/processed_qc.R
@@ -30,30 +30,31 @@ seu <- PercentageFeatureSet(seu,
     FindNeighbors(dims = 1:ndims, verbose = FALSE) %>%
     FindClusters(verbose = FALSE)
 
-counts <- GetAssayData(seu, assay = "SCT", slot = "counts")
-meta <- seu@meta.data
-fdata <- (rownames(counts))
-fdata <- as.data.frame(fdata)
-colnames(fdata) <- "gene_short_name"
-rownames(fdata) <- fdata$gene_short_name
-row.names(counts) <- row.names(fdata)
-colnames(counts) <- row.names(meta)
-pdata <- meta
+seu$qc_cluster_ext_type <- "Not_predicted"
+# counts <- GetAssayData(seu, assay = "SCT", slot = "counts")
+# meta <- seu@meta.data
+# fdata <- (rownames(counts))
+# fdata <- as.data.frame(fdata)
+# colnames(fdata) <- "gene_short_name"
+# rownames(fdata) <- fdata$gene_short_name
+# row.names(counts) <- row.names(fdata)
+# colnames(counts) <- row.names(meta)
+# pdata <- meta
 
-pd <- new("AnnotatedDataFrame", data = pdata)
-fd <- new("AnnotatedDataFrame", data = fdata)
-cds_seu <- newCellDataSetProb(counts, phenoData = pd, featureData = fd)
-cds_seu <- estimateSizeFactors(cds_seu)
-pData(cds_seu)$garnett_cluster <- pData(cds_seu)[, "seurat_clusters"]
+# pd <- new("AnnotatedDataFrame", data = pdata)
+# fd <- new("AnnotatedDataFrame", data = fdata)
+# cds_seu <- newCellDataSetProb(counts, phenoData = pd, featureData = fd)
+# cds_seu <- estimateSizeFactors(cds_seu)
+# pData(cds_seu)$garnett_cluster <- pData(cds_seu)[, "seurat_clusters"]
 
-predicted_cds <- classify_cells(cds_seu, trained_classifier,
-                                db = org.Hs.eg.db,
-                                cluster_extend = TRUE,
-                                cds_gene_id_type = "SYMBOL",
-                                verbose = TRUE)
-predictions <- pData(predicted_cds)[,c("cluster_ext_type")]
-colnames(predictions) <- paste0("qc_",colnames(predictions))
-seu <- AddMetaData(seu, predictions)
+# predicted_cds <- classify_cells(cds_seu, trained_classifier,
+#                                 db = org.Hs.eg.db,
+#                                 cluster_extend = TRUE,
+#                                 cds_gene_id_type = "SYMBOL",
+#                                 verbose = TRUE)
+# predictions <- pData(predicted_cds)[,c("cluster_ext_type")]
+# colnames(predictions) <- paste0("qc_",colnames(predictions))
+# seu <- AddMetaData(seu, predictions)
 
 selected_cells <- rownames(seu@meta.data[seu$keep, ])
 seu$pass_QC <- "PASS"
